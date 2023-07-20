bfd11846b378a9512df19d4affc0a6a197b591c9
Koko <903447054@qq.com>
2023-07-20 14:13:43 +0800

    feat: 新建仓库初始化

---
+++ play/app.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>
+    <ea1-button>按钮</ea1-button>
+    <!-- <Button>123</Button> -->
+  </div>
+</template>
+<script lang="ts" setup>
+// import { Button } from '@easyest/components';
+</script>
